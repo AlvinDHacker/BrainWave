86617effd848abcacd0c0e498126841c13604e52
Zane Fernandes <zane.ferns360@gmail.com>
2023-09-08 18:16:20 +0530

    layout eror resolved

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,18 @@
 import '@style/global.css'
 import './globals.css'
 import { Inter } from 'next/font/google'
-layout
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'BrainWave',
-  description: 'Building your Career with Knowledge, Securing your Career with Blockchain',
+  description:
+    'Building your Career with Knowledge, Securing your Career with Blockchain',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
-        <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
